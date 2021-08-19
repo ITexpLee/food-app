4875ff55b3075aa6e7a168bf54f278e6266c908a
Mikel8Lee <connerrobert927@gmail.com>
2021-08-19 16:50:03 +0530

    Adding multiple smaller components for the Meals component

---
+++ src/components/Meals/AvailableMeals.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Importing Css file and Assets
+import classes from "./AvailableMeals.module.css";
+
+// Right now we will be using dummy data but on this page we will connect with databse
+// We will fetch the Meals from the Database on this page which will finally Render in Meals Component
+const AvailableMeals = () => {};
+
+export default AvailableMeals;
