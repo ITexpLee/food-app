d1f676c6bf902ad926b6feacc8b7a1b72b256146
Mikel8Lee <connerrobert927@gmail.com>
2021-08-26 03:15:02 +0530

    adding animations with useEffect and useState also using clearing functions

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 // Importing css and assets
 import classes from "./HeaderCartButton.module.css";
@@ -6,17 +6,45 @@ import CartIcon from "../Cart/CartIcon.jsx";
 import CartContext from "../../store/CartContext";
 
 const HeaderCartButton = (props) => {
+  // useState is used to Re-Render the entire class
+  const [itemAdded, setItemAdded] = useState(false);
+
   // Using context for the button
   const ctx = useContext(CartContext);
+  const { items } = ctx;
 
   // Counting the number of items in Cart
   // Here currentNumber is an accumulator repeated everytime
-  const numberOfItems = ctx.items.reduce((currentNumber, item) => {
+  const numberOfItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  // Adding animation to React
+  const btnClasses = `${classes.button} ${itemAdded ? classes.bump : ""}`;
+
+  // useEffect for animations
+  useEffect(() => {
+    // if the cart is empty we don't want a bump
+    // It is because useEffect is always run the first time page Render
+    // We run useEffect whenever item changes
+    if (items.length === 0) {
+      return;
+    }
+    setItemAdded(true);
+    // Create a step Timeout which removes the class
+    const timer = setTimeout(() => {
+      setItemAdded(false);
+    }, 200);
+
+    // Clearing the previous Timer
+    // it is only run when the code is executed for second time
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
