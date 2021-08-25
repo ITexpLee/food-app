cdb6328c2ffd99285e397a4ae81589091f870352
Mikel8Lee <connerrobert927@gmail.com>
2021-08-26 01:42:06 +0530

    Adding cart-item component to render all item. Adding the logic with bind to make sure. It returns the right id

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // Importing Custom components
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../store/CartContext.jsx";
 
 // Importing css and assets
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  // Using Context to render the cart
+  const cartCtx = useContext(CartContext);
+
+  // Order button logic
+  const hasItems = cartCtx.items.length > 0;
+
+  // Getting all the information. Fixing it to two decimal places
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  // Cart Item Removing and Adding handler
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   // We have created CartItems outside of the return because we want the logic to be outside
   // All this code will be broken into component iteself.
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -22,13 +45,14 @@ const Cart = (props) => {
       {cartItems}
       <p className={classes.total}>
         <span>Total Amount</span>
-        <span>39.62</span>
+        <span>{totalAmount}</span>
       </p>
       <article className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onHideCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {/* Dynamically checking if the cart has items */}
+        {hasItems && <button className={classes.button}>Order</button>}
       </article>
     </Modal>
   );
---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// importing css module and assets
+import classes from "./CartItem.module.css";
+
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <article>
+        <h2>{props.name}</h2>
+        <p className={classes.summary}>
+          <span className={classes.price}>{price}</span>
+          <span className={classes.amount}>{props.amount}</span>
+        </p>
+      </article>
+      <article className={classes.action}>
+        <button onClick={props.onRemove}>-</button>
+        <button onClick={props.onAdd}>+</button>
+      </article>
+    </li>
+  );
+};
+
+export default CartItem;
