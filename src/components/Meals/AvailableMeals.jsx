b416338ebe1640722b566c74110e7ce0f85f30ae
Mikel8Lee <connerrobert927@gmail.com>
2021-09-02 17:06:17 +0530

    error and loading state handling in available meals

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Importing other components
 import Card from "../UI/Card";
@@ -6,41 +6,70 @@ import MealItem from "./MealItem/MealItem.jsx";
 
 // Importing Css file and Assets
 import classes from "./AvailableMeals.module.css";
+import axios from "axios";
 
-// Right now we will be using dummy data but on this page we will connect with databse
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
-// We will fetch the Meals from the Database on this page which will finally Render in Meals Component
 const AvailableMeals = () => {
+  // Managing the state of meals
+  const [meals, setMeals] = useState([]);
+  // Managing the loading state
+  const [isLoading, setIsLoading] = useState(true);
+  // Managing the Erorr State
+  const [hasError, setHasErorr] = useState(null);
+
+  // We will fetch the Meals from the Database on this page which will finally Render in Meals Component
+  useEffect(() => {
+    // As we cannot convert the useEffect function to async
+    const fetchMeals = async () => {
+      try {
+        const res = await axios.get(
+          "https://react-http-63562-default-rtdb.firebaseio.com/meals.json"
+        );
+        // Mapping the parsed JSON data in an object
+        const loadedMeals = [];
+        // Loop through all the keys in data
+        // Here each key id contains a movie object with name,description,price.. We store this entire thus keys: value in array;
+        for (let key in res.data) {
+          loadedMeals.push({
+            id: key,
+            name: res.data[key].name,
+            description: res.data[key].description,
+            price: res.data[key].price,
+          });
+        }
+        // Set our Meals state to be loadedMeals array
+        setMeals(loadedMeals);
+      } catch (error) {
+        setHasErorr(`Something went Error: ${error.message}!!`);
+      }
+    };
+    // executing fetch meals
+    fetchMeals();
+    // Turn isLoading to false since the set Meals state change is completed
+    setIsLoading(false);
+  }, []);
+
+  // Setting content variable depending on the state of meals (loaded or not)
+  // We can also simply return some other jsx code depending on error or loading
+
+  if (isLoading) {
+    return (
+      <section className={classes["meals-loading"]}>
+        <p>...is Loading</p>
+      </section>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <section className={classes["meals-error"]}>
+        <p>{hasError}</p>
+      </section>
+    );
+  }
+
   // We always want to return a lean JSX code thus the logic is kept outside
   // We want a small jsx component for every meal which we will do by mapping
-  let mealsList = DUMMY_MEALS.map((meal) => (
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
